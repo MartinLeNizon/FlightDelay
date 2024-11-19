d6ef9ca06ff7b13e842c98d94faf99a9672213c3
MartinLeNizon <nizonmartin@gmail.com>
2024-11-19 16:31:40 +0100

    Add data visualizers

---
+++ scripts/viewers/data_viewer_flight.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Load the flight data
+flight_file_path = './data/flight_data/flights_sample_3m.csv'
+flights = pd.read_csv(flight_file_path)
+
+# Preview the data
+print(flights.head())
+print(flights.info())
