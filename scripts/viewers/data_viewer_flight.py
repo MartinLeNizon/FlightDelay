736647a9286f1bf34ac2ce006782548f3a9904b9
MartinLeNizon <nizonmartin@gmail.com>
2024-11-21 18:22:38 +0100

    Refactoring Docker

--- scripts/viewers/data_viewer_flight.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Load the flight data
-flight_file_path = '../data/raw/flight_data/flights_sample_3m.csv'
-flights = pd.read_csv(flight_file_path)
-
-# Preview the data
-print(flights.head())
-print(flights.info())
