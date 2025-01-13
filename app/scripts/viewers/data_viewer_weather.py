ff4621c8f735d9f18ef9ff572a644c11d58f84d5
MartinLeNizon <nizonmartin@gmail.com>
2025-01-13 11:00:31 +0100

    Remove app folder

--- app/scripts/viewers/data_viewer_weather.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Load the weather data
-weather_file_path = '../data/raw/weather_data/largest_us_cities_weatherdata_2020_2023.csv'
-weather = pd.read_csv(weather_file_path)
-
-# Preview the data
-print(weather.head())
-print(weather.info())
